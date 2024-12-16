06a7753420c19fd935fbced232dcba2b9822d2e7
Tatyana Karlen <tatyanakarlen@Tatyanas-MacBook-Pro.local>
2024-12-16 13:17:05 -0500

    sorted the targetAudience problem in details page

--- src/app/featuredprojects/[slug]/page.js
+++ src/app/featuredprojects/[slug]/page.js
@@ -1,27 +1,115 @@
+"use client";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { useParams } from "next/navigation";
 import { featuredProjects } from "@/app/data/FeaturedProjects";
+import { Container, Row, Col } from "react-bootstrap";
+import styles from "./page.module.css";
 
 export default function ProjectDetails({ params }) {
-  const { slug } = params;
+  const { slug } = useParams();
 
-  // Match the project based on the slug
   const project = featuredProjects.find(
     (proj) => proj.title.toLowerCase().replace(/ /g, "-") === slug
   );
 
-  // Handle not found
   if (!project) {
     return <div>Project not found</div>;
   }
+  //   const { slug } = params;
+
+  //   const project = featuredProjects.find(
+  //     (proj) => proj.title.toLowerCase().replace(/ /g, "-") === slug
+  //   );
+
+  //   if (!project) {
+  //     return <div>Project not found</div>;
+  //   }
+
+  // working on this
+  //   const router = useRouter();
+  //   const [project, setProject] = useState(null);
+
+  //   useEffect(() => {
+  //     if (router.isReady && router.query.slug) {
+  //       const slug = router.query.slug;
+
+  //       const proj = featuredProjects.find(
+  //         (proj) => proj.title.toLowerCase().replace(/ /g, "-") === slug
+  //       );
+
+  //       setProject(proj);
+  //     }
+  //   }, [router.isReady, router.query.slug]);
+
+  //   if (!router.isReady || !project) {
+  //     return <div>Project not found</div>;
+  //   }
 
   return (
-    <div>
-      <h1>{project.title}</h1>
-      <p>{project.year}</p>
-      <img src={project.image} alt={project.title} />
-      <p>{project.text}</p>
-      <a href={project.link} target="_blank" rel="noopener noreferrer">
-        Visit Project
-      </a>
+    <div className="section">
+      <Container>
+        <h2 className="xtraBold">Project: {project && project.title}</h2>
+        <p className="mt-4">{project && project.sections.intro}</p>
+        <h5 className="mt-5 xtraBold">Project details</h5>
+        <hr className="mt-4"></hr>
+        <Row className="py-2">
+          <Col>
+            <div className="d-flex flex-column">
+              <span className="text-secondary">Year</span>
+              <span className="fw-bold">{project && project.year}</span>
+            </div>
+          </Col>
+          <Col>
+            {" "}
+            <div className="d-flex flex-column">
+              <span className="text-secondary">Industry</span>
+              <span className="fw-bold">{project.industry}</span>
+            </div>
+          </Col>
+        </Row>
+        <hr></hr>
+        <Row className="py-2">
+          <Col>
+            <div className="d-flex flex-column">
+              <span className="text-secondary">Project team</span>
+              <span className="fw-bold">Onsite or remote</span>
+            </div>
+          </Col>
+        </Row>
+
+        <h5 className="mt-5 xtraBold">Target Audience</h5>
+        <Row className="mt-4 pb-2">
+        {project.targetAudience &&
+          Object.entries(project.targetAudience).map(
+            ([audience, percentage], index) => (
+              <Col key={index}>
+                <div
+                  className={`${styles.detailsCard} d-flex flex-column align-items-center justify-content-center p-3 rounded`}
+                >
+                  <span className="xtraBold fs-2">{percentage}%</span>
+                  <span className="text-secondary mt-1">{audience}</span>
+                </div>
+              </Col>
+            )
+          )}
+      </Row>
+        <Row className="mt-4">
+          <h5 className="xtraBold">What problem are you solving?</h5>
+          <p className="mt-2">{project && project.sections.problem}</p>
+        </Row>
+        <Row className="mt-4">
+          <h5 className="xtraBold">How are you solving the problem</h5>
+          <p className="mt-2">{project && project.sections.solution}</p>
+        </Row>
+
+        {/* <p>{project.year}</p>
+        <img src={project.image} alt={project.title} />
+        <p>{project.text}</p>
+        <a href={project.link} target="_blank" rel="noopener noreferrer">
+          Visit Project
+        </a> */}
+      </Container>
     </div>
   );
 }
